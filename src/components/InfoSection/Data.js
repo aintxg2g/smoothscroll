560b76940face722f3f5430212003a49eb1d0769
aintxg2g <98034229+aintxg2g@users.noreply.github.com>
2022-02-10 19:53:00 +0530

    Add files via upload

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transactions with zero fees',
+  description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: require('../../images/svg1.svg'),
+  alt: 'Car',
+  dark: false,
+  primary: false,
+  darkText: true        
+
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at anytime',
+  description: 'We have covered no matter where you are located. All you need is an internet connection and a phone or computer',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: require('../../images/svg2.svg'),
+  alt: 'PiggyBank',
+  dark: true,
+  primary: true,
+  darkText: false         
+
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headline: 'Created an account is extremely easy',
+  description: "Get everything set up and ready in under 10 minutes. all you need to do is add your information and you're ready to go." ,
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../images/svg3.svg'),
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true       
+
+};
